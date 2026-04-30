62cf71177ef9f455a9e75c22c51e859d7551bb23
ian <ianjamesburke@gmail.com>
2026-04-29 21:21:54 -0400

    Versioning: --version flag, dynamic __version__, update --reinstall, README install/update section (#16)

--- src/parallax/__init__.py
+++ src/parallax/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.1"
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
+
+try:
+    __version__ = _pkg_version("parallax")
+except PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
